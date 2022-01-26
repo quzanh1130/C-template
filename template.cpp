6e13142896cd4362f0d77d23dfa5c7e5f4e24cd9
Nguyen Quoc Anh <kaisoul224@gmail.com>
2022-01-26 14:00:23 +0700

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,25 @@
+// NGUYEN QUOC ANH
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define ff first
+#define ss second
+#define enld endl
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fort(i, a, b) for(int i = a; i <= b; ++i)
+#define fortt(i, a, b) for (int i = a; i >= b; --i)
+
+const long double  pi = 4.0*atanl(1.0);
+const int oo = 100000000;
+const int maxN=200005;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    ...
+    return 0;
+}
